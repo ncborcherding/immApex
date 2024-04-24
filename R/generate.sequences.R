c48e97a11ec99b6ccbb6f6e2797db311e2a0270c
theHumanBorch <ncborch@gmail.com>
2024-04-24 11:01:07 -0500

    generate and tokenize

---
+++ R/generate.sequences.R
@@ -0,0 +1,53 @@
+#' Randomly generate amino acid seqeunces
+#' 
+#' Use this to make synthetic amino acid sequences
+#' for purposes of testing code, training models,
+#' or noise.
+#' 
+#' @examples
+#' generate.sequences(prefix.motif = "CAS",
+#'                    suffix.motif = "YF",
+#'                    number.of.sequences = 100,
+#'                    min.length = 8,
+#'                    max.length = 16)
+#'                         
+#' @param prefix.motif Add defined amino acid sequence to the start of the generated sequences.
+#' @param suffix.motif Add defined amino acid sequence to the end of the generated sequences
+#' @param number.of.sequence Number of sequences to generate
+#' @param sequence.dictionary The letters to use in sequence generation (default are all amino acids)
+#' @param min.length Minimum length of the final sequence (will be adjusted if 
+#' incongruent with prefix.motif/suffix.motif)
+#' @max.length Maximum length of the final sequence
+#' 
+#' @export
+#' @importFrom stringi function stri_rand_strings
+#' 
+#' @return A vector of generated seqeunces
+generate.sequences <- function(prefix.motif = NULL,
+                               suffix.motif = NULL,
+                               number.of.sequences = 100,
+                               sequence.dictionary = c("A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"),
+                               min.length = 1,
+                               max.length = 10) {
+  if(!is.null(prefix.motif) | !is.null(suffix.motif)) {
+    motif.length <- sum(nchar(c(prefix.motif,suffix.motif)))
+    if(min.length < motif.length) {
+      print("New min.length = ", motif.length, " based on the selection of motifs.")
+      min.length <- 0
+    }
+    max.length <- max.length-motif.length
+  }
+  length.range <- seq(min.length, max.length)
+  seq.length <- sample(length.range, number.of.sequences, replace = TRUE)
+  
+  # Generate the random sequences
+  lapply(seq_len(number.of.sequence), function(x) {
+      sequence <- stri_rand_strings(n = 1, length = seq.length[x], pattern = paste0("[", paste(sequence.dictionary, collapse = ""), "]"))
+      #add prefix and suffix
+      if(!is.null(prefix.motif) | !is.null(suffix.motif)) {
+        sequence <- paste(prefix.motif, sequence, suffix.motif, sep = "")
+      }
+  }) -> sequences
+  sequences <- unlist(sequences)
+  return(sequences)
+}
---
+++ R/tokenize.sequences.R
@@ -0,0 +1,69 @@
+#' Generate Tokenized Sequences from Amino Acid String
+#' 
+#' Use this to transform amino acid sequences into 
+#' tokens in preparing for deep learning models. 
+#' 
+#' @examples
+#' new.sequences <- generate.sequences(prefix.motif = "CAS",
+#'                                     suffix.motif = "YF",
+#'                                     number.of.sequences = 100,
+#'                                     min.length = 8,
+#'                                     max.length = 16)
+#'                           
+#'sequence.matrix <- tokenize.sequences(new.sequences, 
+#'                                      add.startstop = TRUE,
+#'                                      start.token = "!",
+#'                                      stop.token = "^", 
+#'                                      convert.to.matrix = TRUE)
+#'                         
+#' @param input.sequences The amino acid sequences to use.
+#' @param add.startstop Add start and stop tokens to the sequence
+#' @param start.token The character to use for the start token
+#' @param stop.token The character to use for the stop token
+#' @param max.length Additional length to pad, NULL will pad sequences 
+#' to the max length of input.sequences
+#' @param convert.to.matrix Return a matrix (TRUE) or a vector (FALSE)
+#' 
+#' @export
+#' @return Tokenized amino acid sequences in a matrix of vector
+tokenize.sequences <- function(input.sequences, 
+                               add.startstop = TRUE,
+                               start.token = "!",
+                               stop.token = "^", 
+                               max.length = NULL,
+                               convert.to.matrix = TRUE) {
+  
+  if(add.startstop) {
+    char_set <- c(start.token,amino.acids, stop.token)
+    print("Adding start and stop tokens...")
+    sequences_updated <- sapply(input.sequences, function(x) paste(start.token, x, stop.token, sep = ""))
+  } else {
+    char_set <- c(amino.acids)
+  }
+  
+  # Create a mapping of amino acids to integers
+  char_to_int <- setNames(seq_along(char_set), char_set)
+  
+  print("Tokenizing sequences...")
+  sequences_tokenized <- lapply(sequences_updated, function(seq) {
+    as.vector(char_to_int[strsplit(seq, "")[[1]]])
+  })
+  if(is.null(max.length)) {
+    max.length <- max(nchar(sequences_updated))
+  }
+  
+  print("Padding sequences...")
+  sequences_tokenized <- .padded.strings(sequences_tokenized, max.length)
+  
+  if(convert.to.matrix) {
+    print("Preparing a tokenized matrix...")
+    sequences_matrix <- t(sapply(sequences_tokenized, function(x) x))
+    return(sequences_matrix)
+  } else {
+    return(sequences_tokenized)
+  }
+}
+
+
+
+
