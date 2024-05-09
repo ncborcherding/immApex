7fed9ec4031545531973f405ff656011016986b5
theHumanBorch <ncborch@gmail.com>
2024-05-09 15:11:58 -0500

    Add gene list from IMGT

--- R/data.R
+++ R/data.R
@@ -8,3 +8,14 @@
 #' @name apex_AA_data
 #' 
 NULL
+
+#' A list of IMGT gene names by genes, loci, and species
+#' 
+#' @description A list of gene names to use for converting for 
+#' uniformity. Data pulled from IMGT.org 09/05/2024.
+#'  
+#' @docType data
+#' @concept Data
+#' @name apex_gene.list
+#' 
+NULL
