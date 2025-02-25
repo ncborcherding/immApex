905f2dd724652b3173e7448cef7e2012f9e41dc0
theHumanBorch <ncborch@gmail.com>
2025-02-25 08:13:20 -0600

    modify testing protocols

--- tests/testthat/test-variationalSequences.R
+++ tests/testthat/test-variationalSequences.R
@@ -2,10 +2,11 @@
 # 1. Check module availability once.
 keras_installed <- reticulate::py_module_available("keras")
 numpy_installed <- reticulate::py_module_available("numpy")
+keras3_installed <- packageVersion("keras3") < "1.2.0"
 
 # 2. If not installed, skip everything:
-if (!keras_installed || !numpy_installed) {
-  test_that("Skipping generateSequences tests", {
+if (!keras_installed || !numpy_installed || !keras3_installed) {
+  test_that("Skipping variationalSequences tests", {
     skip("Required Python modules (Keras, NumPy) are not available.")
   })
 } else {
