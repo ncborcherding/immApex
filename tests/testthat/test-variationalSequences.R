9ed32d02e3657961556e40f9ebaa09d459b8ac0f
theHumanBorch <ncborch@gmail.com>
2025-06-10 08:33:35 -0500

    Update test-variationalSequences.R

--- tests/testthat/test-variationalSequences.R
+++ tests/testthat/test-variationalSequences.R
@@ -1,37 +1,41 @@
 # test script for variationalSequences.R - testcases are NOT comprehensive!
-test_that("variationalSequences works", {
-      set.seed(42)
-      test_that("Test for valid input sequence length", {
+
+set.seed(42)
+sequences <-  generateSequences(prefix.motif = "CAS",
+                                suffix.motif = "YF",
+                                number.of.sequences = 100,
+                                min.length = 8,
+                                max.length = 16)
+                         
+test_that("Test for valid input sequence length", {
         expect_error(variationalSequences(input.sequences = character(0)),
                      "input.sequences must have at least one sequence.")
-      })
+})
       
-      test_that("Test for valid encoder function", {
-        sequences <- c("CASGY", "CASDY", "CASTY")
+test_that("Test for valid mode", {
         expect_error(variationalSequences(input.sequences = sequences,
-                                          encoder.function = "invalidEncoder"),
-                     "Invalid encoder provided.")
-      })
+                                          mode = "invalidEncoder", 
+                                          verbose = FALSE),
+                    regexp = "onehot")
+})
       
-      test_that("Test for valid optimizer", {
-        sequences <- c("CASGY", "CASDY", "CASTY")
+test_that("Test for valid optimizer", {
         expect_error(variationalSequences(input.sequences = sequences,
                                           optimizer = "invalidOptimizer"),
                      "Please select a compatible optimizer function in the Keras R implementation.")
-      })
+})
       
-      test_that("Test for correct output type", {
-        sequences <- getdata("generateSequences", "generateSequences_T1")[1:100]
+test_that("Test for correct output type", {
         result <- variationalSequences(input.sequences = sequences, 
                                        sequence.dictionary = amino.acids[1:20])
         expect_true(is.vector(result))
-      })
+})
       
-      test_that("Test for correct sequence generation", {
-        sequences <- getdata("generateSequences", "generateSequences_T1")[1:100]
+test_that("Test for correct sequence generation", {
         number_of_sequences <- 5
         result <- variationalSequences(input.sequences = sequences, 
-                                       number.of.sequences = number_of_sequences)
+                                       number.of.sequences = number_of_sequences, 
+                                       verbose = FALSE)
         expect_equal(length(result), number_of_sequences)
-      })
 })
+
